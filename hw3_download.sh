cd77a398f752264657a0ec9c9e9cb34f345f5dae
weiyuan-c <87663784+weiyuan-c@users.noreply.github.com>
2022-12-16 13:57:12 +0800

    Update hw3_download.sh

--- hw3_download.sh
+++ hw3_download.sh
@@ -1,2 +1,2 @@
-wget https://www.dropbox.com/s/mo3u9ht2ws5ysux/model2.pth?dl=1 -O model.pth
+wget https://www.dropbox.com/s/iozgaiq3p0itkmi/model2.pth?dl=1 -O model.pth
 python3 -c "import clip; clip.load('ViT-B/32'); clip.load('ViT-L/14')"
